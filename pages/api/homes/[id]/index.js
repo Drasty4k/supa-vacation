cfe38fefadf86494436b7ce6edeeeedf5afb0526
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-10-08 17:10:37 +0300

    implemented the home delete functionality

--- pages/api/homes/[id]/index.js
+++ pages/api/homes/[id]/index.js
@@ -1,6 +1,13 @@
 import { getSession } from "next-auth/react";
 import { PrismaClient } from "@prisma/client";
 
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
@@ -34,6 +41,23 @@ export default async function handler(req, res) {
       res.status(500).json({ message: "Something went wrong" });
     }
   }
+  // Delete home
+  else if (req.method === "DELETE") {
+    try {
+      const home = await prisma.home.delete({
+        where: { id },
+      });
+      // Remove image from Supabase storage
+      if (home.image) {
+        const path = home.image.split(`${process.env.SUPABASE_BUCKET}/`)?.[1];
+        await supabase.storage.from(process.env.SUPABASE_BUCKET).remove([path]);
+      }
+      res.status(200).json(home);
+    } catch (e) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+
   // HTTP method not supported!
   else {
     res.setHeader("Allow", ["PATCH"]);
