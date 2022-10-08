14f98fa98ed338c3a511ca59e0234abd3df548f9
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-10-08 14:39:19 +0300

    secured the "/create" route

--- pages/create.js
+++ pages/create.js
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import ListingForm from "@/components/ListingForm";
 import axios from "axios";
+import { getSession } from "next-auth/react";
 
 const Create = () => {
   const addHome = (data) => axios.post("/api/homes", data);
@@ -23,5 +24,24 @@ const Create = () => {
     </Layout>
   );
 };
+
+export async function getServerSideProps(context) {
+  // Check if user is authenticated
+  const session = await getSession(context);
+
+  // If not, redirect to the homepage
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default Create;
